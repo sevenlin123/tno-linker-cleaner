eb20f8b57f994f5121d374f2ced24599749058e4
Edward Lin <edward.lin@me.com>
2018-01-16 10:31:07 -0500

    Change reference coordinate of projection

--- deparallax.py
+++ deparallax.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-# deparallax.py, version 0.2.2
+# deparallax.py, version 0.4
 #
 # Remove the TNO parallax motion by transferring the topographic equatorial
 # coordinate to barycentric equatorial coordinate. 
@@ -11,42 +11,60 @@
 # v0.2: added parallax_distance function
 # v0.2.1: fixed negative RA issue
 # v0.2.2: fixed 'nan' mjd issue
+# v0.3: correct the elongation calculation; force R to be an array
+# v0.4: project the coordinate to around 90, 0
 ##########################################################################
 
 import numpy as np
-from skyfield.api import load
+from skyfield.api import load, Topos
 planets = load('de423.bsp')
 
+def arc(RA1, RA2, DEC1, DEC2):
+    dDEC = DEC1-DEC2
+    dRA = RA1-RA2
+    c = (np.sin((dDEC/2.))**2 + np.cos(DEC1)*np.cos(DEC2)*np.sin(dRA/2.)**2)**0.5
+    ARC = 2*np.arcsin(c)
+    return ARC
+    
 def topo_to_bary(tri_pos):
-    ra = tri_pos[0]
-    dec = tri_pos[1]
+    ra = np.array(tri_pos[0])
+    dec = np.array(tri_pos[1])
     mjd = np.array(tri_pos[2])
-    R = tri_pos[3]
+    R = np.array(tri_pos[3])
     mjd[np.isnan(mjd)] = 50000
-    #print ra[4], dec[4], mjd[4], R[4]
     ts = load.timescale()
     t = ts.tai(jd=mjd+2400000.500428)
     earth = planets['earth']
-    x_earth, y_earth, z_earth = earth.at(t).position.au
+    ctio = earth + Topos('30.169 S', '70.804 W', elevation_m=2200)
+    x_earth, y_earth, z_earth = ctio.at(t).position.au #ICRS xyz of earth
     earth_distance = (x_earth**2 + y_earth**2 + z_earth**2)**0.5
     dec_bary = np.arcsin(-z_earth/earth_distance)
     ra_bary = np.arctan2(-y_earth, -x_earth)
-    #print ra_bary, dec_bary
-    elong = ((ra-ra_bary)**2+(dec-dec_bary)**2)**0.5
+    elong = arc(ra, ra_bary, dec, dec_bary)
     delta = earth_distance*np.cos(elong) + (R**2- (earth_distance*np.sin(elong))**2)**0.5
-    x = np.cos(dec)*np.cos(ra)*delta
+    x = np.cos(dec)*np.cos(ra)*delta #equatorial xyz of object
     y = np.cos(dec)*np.sin(ra)*delta
-    z = np.sin(dec)*delta
+    z = np.sin(dec)*delta   
     xS = x + x_earth
     yS = y + y_earth
     zS = z + z_earth
     Rp = (xS**2+yS**2+zS**2)**0.5
-#   print R, delta, earth_distance, x,y,z, x_earth, y_earth, z_earth, xS, yS, zS, Rp
     decS = np.arcsin(zS/Rp)
-    raS = np.arctan2(yS,xS)
-    raS[raS < 0] += 2*np.pi
-    raS[raS > 2*np.pi] -= 2*np.pi
-    return raS, decS
+    raS = np.arctan2(yS,xS)%(2*np.pi)
+    #return raS, decS
+    ra_mean = ra.mean()
+    dec_mean = dec.mean()
+    
+    midraS, middecS = raS.mean(), decS.mean()
+    lat_off = np.radians(5.0)
+    xaxis, yaxis, zaxis = (0, 0), (np.pi/2, 0), (0, np.pi/2)
+    nxaxis, nyaxis, nzaxis = ((midraS-np.pi/2)%(2*np.pi), 0), (midraS, middecS), ((midraS+np.pi)%(2*np.pi), (np.pi/2.0-middecS))
+    nx = np.cos(arc(nxaxis[0], xaxis[0], nxaxis[1], xaxis[1]))*xS + np.cos(arc(nxaxis[0], yaxis[0], nxaxis[1], yaxis[1]))*yS + np.cos(arc(nxaxis[0], zaxis[0], nxaxis[1], zaxis[1]))*zS
+    ny = np.cos(arc(nyaxis[0], xaxis[0], nyaxis[1], xaxis[1]))*xS + np.cos(arc(nyaxis[0], yaxis[0], nyaxis[1], yaxis[1]))*yS + np.cos(arc(nyaxis[0], zaxis[0], nyaxis[1], zaxis[1]))*zS
+    nz = np.cos(arc(nzaxis[0], xaxis[0], nzaxis[1], xaxis[1]))*xS + np.cos(arc(nzaxis[0], yaxis[0], nzaxis[1], yaxis[1]))*yS + np.cos(arc(nzaxis[0], zaxis[0], nzaxis[1], zaxis[1]))*zS
+    ndecS = np.arcsin(nz/Rp)
+    nraS = np.arctan2(ny,nx)%(2*np.pi)
+    return nraS, ndecS
     
 def parallax_distance(ra1, ra2, dec1, dec2, mjd1, mjd2):
     ts = load.timescale()
